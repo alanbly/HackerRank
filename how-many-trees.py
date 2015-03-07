13308dbdb5222e2b2868ca107d45343b8142f9cc
Adam McCormick <alanbly@gmail.com>
2015-03-07 13:22:40 -0800

    Trees work, but it's really slow

--- how-many-trees.py
+++ how-many-trees.py
@@ -5,50 +5,36 @@ from itertools import *
 
 # total trees = N^(N-2)
 
-# T(N, 1) = (N-1)!
-# T(N, N-1) = 1
-# T(N, L) = T(N-1, L)*(N-1-1 + L + L-1)  > N+2L-3                                                            + T(N-1, L-1)*(N-1-(L-1))
-#           T(N-1, L) with the new node above each but the root, below each leaf, or in place of each branch + number with one less leaf and one less node with the new node added to each non-leaf node
+# T(N, 2) = N!/2
+# T(N, N-1) = N
+# T(N, L) = T(N-1, L) with a new node in place of each   
 
-# T(4, 2) = 1        *()  >5<                                                                                + 2*(3-1) >4<
-# T(5, 2) = 9        *()  >6<                                                                                + 6*(4-1) >18<  + 6?
+# T(4, 2) = 24/2 = 12
+# T(5, 2) = 120/2 = 60
 
-# T(3, {1, 2}) = {2, 1}
-# T(4, {1, 2, 3}) = {6, 9, 1}
-# T(5, {1, 2, 3, 4}) = {24, 60, , 1}
+# T(3, {2}) = {3}
+# T(4, {2, 3}) = {12, 4}
+# T(5, {2, 3, 4}) = {60, 60, 5}
+# T(6, {2, 3, 4, 5}) = {360, , , 6}
 
-def treesRecurse(N, L, memo = {}) :
-  if N not in memo :
-    memo[N] = {}
-  if L in memo[N] :
-    return memo[N][L]
-  if N - L == 1 :
-    memo[N][L] = 1
-    return memo[N][L] 
-  if L == 1 : 
-    memo[N][L] = factorial(N-1)
-    return memo[N][L]
-  print "%d*%d + %d" % (treesRecurse(N-1, L, memo), (N+2*L-3), treesRecurse(N-1, L-1, memo)*(N-L))
-  return treesRecurse(N-1, L, memo)*(N+2*L-3) + treesRecurse(N-1, L-1, memo)*(N-L)
 
-treesRecurse(4, 1);
-treesRecurse(4, 2);
-treesRecurse(4, 3);
-treesRecurse(5, 2);
-
+def trees(N) :
+  return product(range(1,N+1), repeat=N-2)
 
 def countLeaves(tree) :
-  return len(tree)+1-len(set(tree))
+  return len(tree)+2-len(set(tree))
 
-def treeBrute(N, L, prufers = {}) :
-  if N not in prufers :
-    prufers[N] = [i for i in product(range(1,N+1), repeat=N-1)]
-  return sum([1 if countLeaves(i) == L else 0 for i in prufers[N]]);
+def treeBrute(N, L) :
+  if L == 2 :
+    return factorial(N)/2
+  prufers = trees(N)
+  return sum([1 if countLeaves(i) == L else 0 for i in prufers]);
 
-treeBrute(4, 1);
-treeBrute(4, 2);
-treeBrute(4, 3);
-treeBrute(5, 2);
+# treeBrute(4, 2);
+# treeBrute(4, 3);
+# treeBrute(5, 2);
+# treeBrute(5, 3);
+# treeBrute(5, 4);
 
 (N,L) = [int(i) for i in raw_input().split(" ")]
 
